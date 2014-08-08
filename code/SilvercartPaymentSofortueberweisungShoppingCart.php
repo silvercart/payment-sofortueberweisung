9c704acb74d9e71fa3d0e6a4f9530c2475349c37
Sebastian Diel <sdiel@pixeltricks.de>
2014-08-08 15:35:30 +0200

    ENHANCEMENT: Replaced DataObjectDecorator with DataExtension.

--- code/SilvercartPaymentSofortueberweisungShoppingCart.php
+++ code/SilvercartPaymentSofortueberweisungShoppingCart.php
@@ -1,21 +1,8 @@
 <?php
 /**
- * Copyright 2012 pixeltricks GmbH
+ * Copyright 2014 pixeltricks GmbH
  *
  * This file is part of SilverCart.
- *
- * SilverCart is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SilverCart is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with SilverCart.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package SilvercartPaymentSaferpay
  * @subpackage Base
@@ -26,29 +13,23 @@
  *
  * @package SilvercartPaymentSofortueberweisung
  * @subpackage Base
- * @author Sascha Koehler <skoehler@pixeltricks.de>
- * @since 15.11.2012
- * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
- * @copyright 2012 pixeltricks GmbH
+ * @author Sebastian Diel <sdiel@pixeltricks.de>,
+ *         Sascha Koehler <skoehler@pixeltricks.de>
+ * @since 08.08.2014
+ * @copyright 2014 pixeltricks GmbH
+ * @license see license file in modules root directory
  */
-class SilvercartPaymentSofortueberweisungShoppingCart extends DataObjectDecorator {
-
+class SilvercartPaymentSofortueberweisungShoppingCart extends DataExtension {
+    
     /**
-     * Additional datafields and relations.
+     * DB attributes.
      *
-     * @return array
-     *
-     * @author Sascha Koehler <skoehler@pixeltricks.de>
-     * @since 15.11.2012
+     * @var array
      */
-    public function extraStatics() {
-        return array(
-            'db' => array(
-                'sofortueberweisungTransactionID' => 'VarChar(150)',
-                'sofortueberweisungReason'        => 'VarChar(27)'
-            )
-        );
-    }
+    private static $db = array(
+        'sofortueberweisungTransactionID' => 'VarChar(150)',
+        'sofortueberweisungReason'        => 'VarChar(27)',
+    );
 
     /**
      * Returns the Sofortueberweisung reason.
--- plugins/SilvercartPaymentSofortueberweisungOrderPlugin.php
+++ plugins/SilvercartPaymentSofortueberweisungOrderPlugin.php
@@ -1,21 +1,8 @@
 <?php
 /**
- * Copyright 2012 pixeltricks GmbH
+ * Copyright 2014 pixeltricks GmbH
  *
  * This file is part of SilverCart.
- *
- * SilverCart is free software: you can redistribute it and/or modify
- * it under the terms of the GNU General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * SilverCart is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with SilverCart.  If not, see <http://www.gnu.org/licenses/>.
  *
  * @package SilvercartPaymentSaferpay
  * @subpackage Plugins
@@ -26,12 +13,13 @@
  *
  * @package SilvercartPaymentSofortueberweisung
  * @subpackage Plugins
- * @author Sascha Koehler <skoehler@pixeltricks.de>
- * @since 15.11.2012
- * @license http://www.gnu.org/licenses/lgpl.html GNU Lesser General Public License
- * @copyright 2012 pixeltricks GmbH
+ * @author Sebastian Diel <sdiel@pixeltricks.de>,
+ *         Sascha Koehler <skoehler@pixeltricks.de>
+ * @since 08.08.2014
+ * @copyright 2014 pixeltricks GmbH
+ * @license see license file in modules root directory
  */
-class SilvercartPaymentSofortueberweisungOrderPlugin extends DataObjectDecorator {
+class SilvercartPaymentSofortueberweisungOrderPlugin extends DataExtension {
 
     /**
      * Injects the Sofortueberweisung information from the shopping cart into the order
